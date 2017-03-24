30943a9311c515506ce26bcaf7ad5cb751714255
Christopher Baker <me@christopherbaker.net>
2017-03-24 17:14:36 +0000

    Fix window.

--- 05_threeScreens/src/main.cpp
+++ 05_threeScreens/src/main.cpp
@@ -1,8 +1,24 @@
 #include "ofApp.h"
+#include "ofAppGLFWWindow.h"
 
 
 int main()
 {
-    ofSetupOpenGL(1280, 480, OF_WINDOW);
-    ofRunApp(std::make_shared<ofApp>());
+    int numScreens = 3;
+    int screenWidth = 1920;
+    int screenHeight = 1200;
+
+    ofGLFWWindowSettings settings;
+
+    settings.multiMonitorFullScreen = true;
+    settings.resizable = false;
+    settings.width = screenWidth * numScreens;
+    settings.height = screenHeight;
+    settings.windowMode = OF_FULLSCREEN;
+
+    auto window = ofCreateWindow(settings);
+    auto app = std::make_shared<ofApp>();
+
+    ofRunApp(window, app);
+    return ofRunMainLoop();
 }
